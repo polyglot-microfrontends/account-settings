c5bbb957a028349aad2c766ca0000df8b593360a
Joel Denning <joeldenning@gmail.com>
2021-05-13 11:04:57 -0600

    Fix lint error

--- src/components/settings-home.component.js
+++ src/components/settings-home.component.js
@@ -3,6 +3,7 @@ angular
 .component('settingsHome', {
   templateUrl: () => window.accountSettingsBaseDir + 'components/settings-home.template.html',
   controller: ['$rootScope', function ($rootScope) {
+    // eslint-disable-next-line no-console
     console.log('root scope', $rootScope.singleSpaProps)
   }]
 })
